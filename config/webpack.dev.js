5adb849b4f5b44e3510122b7f0bf8fbb1d4f287c
mohammad.manzoor8972@gmail.com <root@LKNDSS0NRG3QD.local>
2020-05-17 05:03:52 +0530

    Created webpack boilerplate

---
+++ config/webpack.dev.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+    entry:{
+        schoolReport: ["./src/main.js", "./src/school.js"],
+        attendance: ["./src/main.js", "./src/school.js"]
+    },
+    mode:"development",
+    output:{
+        filename:"[name].bundle.js",
+        path: path.resolve(__dirname, "../dist")
+    },
+    devServer:{
+        contentBase: "dist"
+    }
+}
