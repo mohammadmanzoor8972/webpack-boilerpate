5bf5145a13bbc3dd9993bd0f49237b0a7952626b
mohammad.manzoor8972@gmail.com <root@LKNDSS0NRG3QD.local>
2020-05-17 15:26:41 +0530

    Added CSS rules and Eror on display

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
     entry:{
         schoolReport: ["./src/main.js", "./src/school.js"],
-        attendance: ["./src/main.js", "./src/school.js"]
+     //   attendance: ["./src/main.js", "./src/school.js"]
     },
     mode:"development",
     output:{
@@ -11,6 +11,18 @@ module.exports = {
         path: path.resolve(__dirname, "../dist")
     },
     devServer:{
-        contentBase: "dist"
+        contentBase: "dist",
+        overlay: true
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    {loader: "style-loader"},
+                    {loader: "css-loader"}
+                ]
+            }
+        ]
     }
 }
